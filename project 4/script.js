4d33b9d679b753f6421375fa441f0960f4802b13
PAPE SEYDOU DIA <diapapeseydou14@gmail.com>
2022-07-24 04:21:41 +0200

    Mise en ligne du premier site

---
+++ project 4/script.js
@@ -0,0 +1,10 @@
+//Type
+var typed = new Typed(".typed", {
+  strings: [
+    "Motivé et déterminé je vise de nouvelles responsabilités",
+    "je désire ainsi évoluer dans le domaine des tics ou tout autre domaine ayant un lien avec l’informatique et la communication. Ecouter et diagnostiquer leurs besoins afin de leur apporter la solution adaptée Appliquer les processus définis et maitriser l’utilisation des outils. Remonter toute information permettant d’identifier un dysfonctionnement ou d’améliorer une procédure Contribuer à la bonne marche du service",
+  ],
+  typeSpeed: 20,
+});
+// AOS
+AOS.init();
